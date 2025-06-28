fb569661ae3b2c45f79dedabc6f6e3c9ef41de26
刘刚 <liugang5@flkj.ai>
2025-06-29 01:13:06 +0800

    es降级

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   base:'/action-machine/',
+  esbuild:{
+    target: 'es2015',
+  },
   plugins: [vue()],
 })
