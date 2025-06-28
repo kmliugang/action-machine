2514aea2d6ab210672cf92939c72dbcd5cae8e95
刘刚 <liugang5@flkj.ai>
2025-06-29 01:52:13 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base:'/action-machine/',
-  esbuild:{
-    target: 'es5',
-  },
-  plugins: [vue()],
-})
+export default defineConfig(({mode})=>({
+  base:mode === 'production' ? '/action-machine/' : '/',
+  plugins: [vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
+  build:{
+    target: 'es2015',
+  }
+}))
