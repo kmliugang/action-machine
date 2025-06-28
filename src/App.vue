b60e7675a7b6a82a6caef921ccee49dd277e9301
刘刚 <liugang5@flkj.ai>
2025-06-29 00:57:22 +0800

    添加图片

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
 <template>
   <div style="color: var(--primary-color);">工作流--预览</div>
   <button>按钮</button>
+  <div>
+    <img style="object-fit: scale-down;" src="./assets/1710122079214354.jpg" alt="">
+  </div>
  
 </template>
 
